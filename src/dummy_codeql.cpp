97a6efc56d2aaf14c0ffd2f6a99067fdcdbaa65b
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 07:32:07 +0200

    Create dummy_codeql.cpp

---
+++ src/dummy_codeql.cpp
@@ -0,0 +1,12 @@
+// Fichier utilisé uniquement pour déclencher CodeQL (analyse de code C++)
+#include <string>
+
+std::string getVersion()
+{
+    return "1.0.0";
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
