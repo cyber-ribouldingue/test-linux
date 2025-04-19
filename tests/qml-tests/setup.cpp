74cf8348ecf37fcbaf43dd7cbf070527df26e58d
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-04-19 07:34:57 +0200

    Create setup.cpp

---
+++ tests/qml-tests/setup.cpp
@@ -0,0 +1,16 @@
+#include "setup.h"
+
+void Setup::applicationAvailable()
+{
+    // Rien à faire ici pour l'instant
+}
+
+void Setup::qmlEngineAvailable(QQmlEngine *engine)
+{
+    // Pas d'action spécifique pour l'instant
+}
+
+void Setup::cleanupTestCase()
+{
+    // Rien à nettoyer pour l'instant
+}
